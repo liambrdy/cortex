847d39e5ee47f97e1132e0f8d1e431853a849e43
Liam Brady <liam.james.brady@gmail.com>
2023-03-22 05:35:27 -0400

    began shader creation

--- src/cortex_opengl.cpp
+++ src/cortex_opengl.cpp
@@ -10,6 +10,14 @@ typedef void gl_bind_buffer(GLenum target, GLuint buffer);
 typedef void gl_buffer_data(GLenum target, GLsizeiptr size, const void * data, GLenum usage);
 typedef GLuint gl_create_shader(GLenum shaderType);
 typedef GLuint gl_create_program();
+typedef void gl_shader_source(GLuint shader, GLsizei count, const GLchar **string, const GLint *length);
+typedef void gl_compile_shader(GLuint shader);
+typedef void gl_get_shader_iv(GLuint shader, GLenum pname, GLint *params);
+typedef void gl_get_shader_info_log(GLuint shader, GLsizei maxLength, GLsizei *length, GLchar *infoLog);
+typedef void gl_get_program_iv(GLuint program, GLenum pname, GLint *params);
+typedef void gl_get_program_info_log(GLuint program, GLsizei maxLength, GLsizei *length, GLchar *infoLog);
+typedef void gl_attach_shader(GLuint program, GLuint shader);
+typedef void gl_link_program(GLuint program);
 
 global gl_create_vertex_arrays *glCreateVertexArrays;
 global gl_bind_vertex_array *glBindVertexArray;
@@ -18,6 +26,14 @@ global gl_bind_buffer *glBindBuffer;
 global gl_buffer_data *glBufferData;
 global gl_create_shader *glCreateShader;
 global gl_create_program *glCreateProgram;
+global gl_shader_source *glShaderSource;
+global gl_compile_shader *glCompileShader;
+global gl_get_shader_iv *glGetShaderiv;
+global gl_get_program_iv *glGetProgramiv;
+global gl_get_shader_info_log *glGetShaderInfoLog;
+global gl_get_program_info_log *glGetProgramInfoLog;
+global gl_attach_shader *glAttachShader;
+global gl_link_program *glLinkProgram;
 
 void LoadAllOpenGLFunctions(platform_get_opengl_function *getOpenGLFunction)
 {
@@ -30,7 +46,50 @@ void LoadAllOpenGLFunctions(platform_get_opengl_function *getOpenGLFunction)
         glBufferData = (gl_buffer_data *)getOpenGLFunction("glBufferData");
         glCreateShader = (gl_create_shader *)getOpenGLFunction("glCreateShader");
         glCreateProgram = (gl_create_program *)getOpenGLFunction("glCreateProgram");
+        glShaderSource = (gl_shader_source *)getOpenGLFunction("glShaderSource");
+        glCompileShader = (gl_compile_shader *)getOpenGLFunction("glCompileShader");
+        glGetShaderiv = (gl_get_shader_iv *)getOpenGLFunction("glGetShaderiv");
+        glGetProgramiv = (gl_get_program_iv *)getOpenGLFunction("glGetProgramiv");
+        glGetShaderInfoLog = (gl_get_shader_info_log *)getOpenGLFunction("glGetShaderInfoLog");
+        glGetProgramInfoLog = (gl_get_program_info_log *)getOpenGLFunction("glGetProgramInfoLog");
+        glAttachShader = (gl_attach_shader *)getOpenGLFunction("glAttachShader");
+        glLinkProgram = (gl_link_program *)getOpenGLFunction("glLinkProgram");
     }
+}
+
+uint32 CreateShader(char *vertSource, char *fragSource)
+{
+    uint32 vert = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert, 1, &vertSource, 0);
+    glCompileShader(vert);
+
+    int32 status = 0;
+    glGetShaderiv(vert, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(vert, 512, 0, infoLog);
+        printf("Vertex shader error: %s\n", infoLog);
+    }
+
+    uint32 frag = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag, 1, &fragSource, 0);
+    glCompileShader(frag);
+
+    glGetShaderiv(frag, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(vert, 512, 0, infoLog);
+        printf("Fragment shader error: %s\n", infoLog);
+    }
+
+    uint32 program = glCreateProgram();
+    glAttachShader(program, vert);
+    glAttachShader(program, frag);
+    glLinkProgram(program);
+
+    
 }
 
 void CreateOpenGLShaders(uint32 *shaders)
